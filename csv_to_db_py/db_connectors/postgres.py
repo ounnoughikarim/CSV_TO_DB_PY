68cdc421bbcbaa69c949b7304d9ab2e1582be9af
ounnoughik <ounnoughikarim@gmail.com>
2026-05-05 17:48:28 +0200

    url decoding

--- csv_to_db_py/db_connectors/postgres.py
+++ csv_to_db_py/db_connectors/postgres.py
@@ -1,9 +1,17 @@
-from sqlalchemy import create_engine
+from sqlalchemy import URL, create_engine
 from db_connectors.base import BaseDBConnector
 
 
 class PostgresConnector(BaseDBConnector):
     def connect(self):
         cfg = self.config
-        url = f"postgresql+psycopg2://{cfg['user']}:{cfg['password']}@{cfg['host']}:{cfg['port']}/{cfg['database']}"
+        #url = f"postgresql+psycopg2://{cfg['user']}:{cfg['password']}@{cfg['host']}:{cfg['port']}/{cfg['database']}"
+        url = URL.create(
+            "postgresql+psycopg2",
+            username=cfg["user"],
+            password=cfg["password"],
+            host=cfg["host"],
+            port=cfg["port"],
+            database=cfg["database"],
+        )
         self.engine = create_engine(url)
